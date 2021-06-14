28faf2e66c965945404d0ad043b65f59a5e8670b
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-14 08:31:56 +0800

    TUGAS 7 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,8 @@
+var tinggiBadan = [156,157,158,159,160,161,162,163,164,165];
+for (var i = 0; i < tinggiBadan.length; i++) {
+  console.log(tinggiBadan[i])
+}
+
+for(let x of tinggiBadan) {
+  console.log(x);
+}
